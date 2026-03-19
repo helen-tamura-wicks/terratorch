867aadc1ccbe5c26884a2eedd55c9c36c068dd31
Isabelle Wittmann <isabelle.wittmann1@ibm.com>
2026-03-19 09:12:46 +0100

    update imports
    Signed-off-by: Isabelle Wittmann <isabelle.wittmann1@ibm.com>

--- terratorch/__init__.py
+++ terratorch/__init__.py
@@ -4,6 +4,9 @@
 import os
 os.environ["NO_ALBUMENTATIONS_UPDATE"] = "True"
 
+import terratorch.datasets
+import terratorch.datamodules
 import terratorch.models  # noqa: F401
 from terratorch.models.backbones import *  # register models in registries # noqa: F403
 from terratorch.registry import BACKBONE_REGISTRY, DECODER_REGISTRY, MODEL_FACTORY_REGISTRY, FULL_MODEL_REGISTRY  # noqa: F401
+import terratorch.tasks
